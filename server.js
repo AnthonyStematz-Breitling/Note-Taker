3db7c1d1688565b62f87217758e2adb395cccf44
Anthony Stematz-Breitling <stembreit@gmail.com>
2020-08-27 17:18:51 -0500

    fixed get meathod for notes

--- server.js
+++ server.js
@@ -10,43 +10,27 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+app.get("/notes", function(req, res) { 
+    res.sendFile(path.join(`${__dirname}/public/notes.html`));
 });
 
 app.get("/api/notes", function(req, res) {
     //reads the database
     // and puts the objects into notelistItems array
-    fs.readFile('db.json', (err, data) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) throw err;
-        let note = JSON.parse(data);
-        console.log(note);
-        return note
+        let allNotes = JSON.parse(data);
+        res.json(allNotes)
     });
-    res.sendFile(path.join(__dirname, "notes.html"));
-});
-num = 1
-app.post("/api/notes", function(req,res){
-    num = num++
-    var newNotes = req.body
-    console.log(newNotes)
-    //takes new note
-    //assigns unique id to the note
-    newNotes.id = num
-    fs.appendFile(db.json, JSON.parse(newNotes))
-    //transfer that note into a JSON object
-    //put it into the the data base
-    //return the updated notes s
-
 });
 
-app.delete("/api/notes", function(req, res){
-    var deleteNote = req.body
-    deleteNote.id
     //takes the unique id of this note
     //finds the note with that id in the database
     //removes that note
+app.get("*", function(req, res) {
+    res.sendFile(path.join(`${__dirname}/public/index.html`));
 });
 
 // Starts the server to begin listening
