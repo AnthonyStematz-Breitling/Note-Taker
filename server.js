d12e6a8052da5abff7e132265d579249079e0962
Anthony Stematz-Breitling <stembreit@gmail.com>
2020-08-27 00:00:20 -0500

    added notes and tried to fill in code with what I know

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var path = require("path");
-const { fstat } = require("fs");
+var fs = require("fs")
 
 // Sets up the Express App
 // =============================================================
@@ -16,16 +16,37 @@ app.get("/", function(req, res) {
 });
 
 app.get("/api/notes", function(req, res) {
-    fs.readFile(db.json)
+    //reads the database
+    // and puts the objects into notelistItems array
+    fs.readFile('db.json', (err, data) => {
+        if (err) throw err;
+        let note = JSON.parse(data);
+        console.log(note);
+        return note
+    });
     res.sendFile(path.join(__dirname, "notes.html"));
 });
-
+num = 1
 app.post("/api/notes", function(req,res){
+    num = num++
     var newNotes = req.body
+    console.log(newNotes)
+    //takes new note
+    //assigns unique id to the note
+    newNotes.id = num
+    fs.appendFile(db.json, JSON.parse(newNotes))
+    //transfer that note into a JSON object
+    //put it into the the data base
+    //return the updated notes s
+
 });
 
 app.delete("/api/notes", function(req, res){
-    
+    var deleteNote = req.body
+    deleteNote.id
+    //takes the unique id of this note
+    //finds the note with that id in the database
+    //removes that note
 });
 
 // Starts the server to begin listening
